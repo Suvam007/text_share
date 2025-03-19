6158e25063a4cfdbc2b38fe5add7c8e7dce5eb4a
Suvam007 <121725387+Suvam007@users.noreply.github.com>
2025-03-19 12:09:40 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -118,10 +118,11 @@ return (
     <h3 className="text-lg font-medium text-gray-300 mb-3">Retrieved Text:</h3>
     <pre className="p-4 text-gray-200 bg-black rounded-md border border-gray-700 shadow-inner min-h-12 
                     max-h-60 overflow-auto break-words whitespace-pre-wrap">
-      
+      {retreivedText}
     </pre>
   </div>
 )}
+
 
       </div>
     </div>
