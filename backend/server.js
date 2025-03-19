3aa7ebd5d5c628cc20399d0ffa4b6927196c7401
Suvam007 <121725387+Suvam007@users.noreply.github.com>
2025-03-19 17:10:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,5 @@ app.use(cors({
 app.use('/api/text',textroutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
